fb16f907223a7fb300350bda7e26bf431171c18f
99003159 <75420482+99003159@users.noreply.github.com>
2020-12-05 12:11:50 +0530

    Update scientific_cal_operations.h
    added age_calculator header function

--- implementation/inc/scientific_cal_operations.h
+++ implementation/inc/scientific_cal_operations.h
@@ -10,5 +10,7 @@ int add(int operand1, int operand2);
 
 float BMI(float weight,float height);
 
+int age_calculator(int present_date, int present_month, int present_year, int birth_date, int birth_month, int birth_year);
+
 #endif  /* #define __SCIENTIFIC_CAL_OPERATIONS_H__ */
 #endif // SCIENTIFIC_CAL_OPERATIONS_H_INCLUDED
