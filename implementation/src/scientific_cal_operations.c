7b14817b004a8d273ac9cff9af81ebbee8cd7566
99002460 <rpavan.raj@ltts.com>
2020-12-05 11:32:58 +0530

    updated operations

--- implementation/src/scientific_cal_operations.c
+++ implementation/src/scientific_cal_operations.c
@@ -1,7 +1,192 @@
-  
-#include <scientific_cal_operations.h>
+ #include <calculator_operations.h>
+int isprime(int operand1);
 
 int add(int operand1, int operand2)
 {
     return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
+}
+
+
+int prime(int operand1)
+{
+  int result = isprime(operand1);
+  return result;
+}
+
+int isprime(int operand1)
+{
+	int i;
+   for(i=2;i<operand1;i++)
+  /* checks prime number */
+  if(operand1%i== 0)
+    break;
+  /* Return 1 for prime number */
+  if(i>operand1/2)
+  return -1;
+  else
+  return 1;
+}
+int pos(int operand1)
+{
+	if(operand1>0)
+		return 1;	
+}
+int neg(int operand1)
+{
+	if(operand1<0)
+		return -1;	
+}
+int iszero(int operand1)
+{
+	if(operand1==0)
+		return 0;	
+}
+int modu(int operand1, int operand2)
+{
+    return operand1 % operand2;
+}
+int eve(int operand1)
+{
+	if(operand1%2==0)
+	{
+		return 1;
+	}
+}
+int odd(int operand1)
+{
+	if(operand1%2==1)
+	{
+		return 1;
+	}
+}
+int perc(int operand1, int operand2)
+{
+    return (operand1*100 /operand2);
+}
+int pmsq(int operand1, int operand2)
+
+{
+
+    return operand1*2 + operand2*2;
+
+}
+int pmrq(int operand1)
+{
+	return operand1*4;
+}
+int pmcr(int operand1)
+{
+	return operand1*6;
+}
+int pmtr(int operand1, int operand2)
+
+{
+
+    return operand1 + operand2;
+
+}
+int arrq(int operand1, int operand2)
+
+{
+
+    return operand1* operand2;
+
+}
+int arsq(int operand1)
+
+{
+	return operand1* operand1;
+}
+int arci(int operand1)
+
+{
+	return operand1* operand1*3;
+}
+int factorial(int operand1)
+
+{
+
+  /* Return -1 for negative numbers */
+
+  if(operand1 < 0)
+
+    return -1;
+
+
+
+  /* Return 1 for 0 */
+
+  if(operand1 == 0)
+
+    return 1;
+
+
+
+  /* Recursively calculate Factorial of the number */
+
+  return operand1 * factorial(operand1-1);
+
+}
+int ams(int operand1)
+{
+	int i,rem=0,res=0;
+	i=operand1;
+	while(i!=0)
+	{
+		rem=i%10;
+		res+=rem*rem*rem;
+		i/=10;
+	}
+	if(res==operand1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+int sq(int operand1)
+{
+	return operand1* operand1;
+}
+int cu(int operand1)
+{
+	return operand1* operand1*operand1;
+}
+int rev(int operand1)
+{
+	int rev=0,rem=0;
+	while(operand1!=0)
+	{
+		rem=operand1%10;
+		rev=rev*10+rem;
+		operand1/=10;
+	}	return rev;
+}
+int volcube(int operand1)
+{
+	return operand1* operand1*operand1;
+}
+int volsp(int operand1)
+{
+	return operand1* operand1*operand1*4;
 }
