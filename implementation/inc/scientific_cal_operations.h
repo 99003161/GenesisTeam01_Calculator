bbb68d4f911ba79673306d57d910ce58483ce57c
SamudralaApurva <samudrala.apurva@ltts.com>
2020-12-05 16:19:04 +0530

    arithmetic

--- implementation/inc/scientific_cal_operations.h
+++ implementation/inc/scientific_cal_operations.h
@@ -5,9 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio_ext.h>
+#include<math.h>
 
 int add(int operand1, int operand2);
-<<<<<<< HEAD
 int subtract(int operand1, int operand2);
 int multiply(int operand1, int operand2);
 int divide(int operand1, int operand2);
@@ -33,12 +33,10 @@ int cu(int operand1);
 int rev(int operand1);
 int volcube(int operand1);
 int volsp(int operand1);
-=======
 
 float BMI(float weight,float height);
 
 int age_calculator(int present_date, int present_month, int present_year, int birth_date, int birth_month, int birth_year);
 
->>>>>>> 455dc37421b38ecc5bf390be322f84290c1b373d
 #endif  /* #define __SCIENTIFIC_CAL_OPERATIONS_H__ */
 #endif // SCIENTIFIC_CAL_OPERATIONS_H_INCLUDED
