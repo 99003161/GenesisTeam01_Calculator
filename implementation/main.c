0dda37b19d96e29a402108b43a56adbaa41c8fb8
99002460 <rpavan.raj@ltts.com>
2020-12-05 10:29:47 +0530

    First commit

--- implementation/main.c
+++ implementation/main.c
@@ -1,13 +1,5 @@
 #include<stdio.h>
 int main()
 {
-    //exit 0;
-    int b=1;
-    //int b=2;
-    int c=3;
-    int d=4;
-    int f=b+d;
-    printf("%d",f);
-    int x=5;
-    int z=420;
+    exit 0;
 }
