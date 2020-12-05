455dc37421b38ecc5bf390be322f84290c1b373d
99003159 <75420482+99003159@users.noreply.github.com>
2020-12-05 12:20:39 +0530

    Update scientific_cal_operations.c
    added age_calc fun

--- implementation/src/scientific_cal_operations.c
+++ implementation/src/scientific_cal_operations.c
@@ -10,3 +10,14 @@ float BMI(float weight,float height)
 {
 	return (weight/(height*height))
 }
+
+int age_calculator(int present_date, int present_month, int present_year, int birth_date, int birth_month, int birth_year) {
+   if (birth_date > present_date) {
+      present_month = present_month - 1;
+   }
+   if (birth_month > present_month) {
+      present_year = present_year - 1;
+   }
+   int final_year = present_year - birth_year;
+   return final_year;
+}
