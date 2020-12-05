3f893f38ab51274d80055075f629c04c85ebe2e2
99002460 <rpavan.raj@ltts.com>
2020-12-05 11:12:50 +0530

    created inc src and test

---
+++ implementation/inc/scientific_cal_operations.h
@@ -0,0 +1,37 @@
+#ifndef CALCULATOR_OPERATIONS_H_INCLUDED
+#define CALCULATOR_OPERATIONS_H_INCLUDED
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+int add(int operand1, int operand2);
+int subtract(int operand1, int operand2);
+int multiply(int operand1, int operand2);
+int divide(int operand1, int operand2);
+int prime(int operand1);
+int pos(int operand1);
+int neg(int operand1);
+int iszero(int operand1);
+int modu(int operand1, int operand2);
+int eve(int operand1);
+int odd(int operand1);
+int perc(int operand1, int operand2);
+int pmsq(int operand1, int operand2);
+int pmrq(int operand1);
+int pmrq(int operand1);
+int pmcr(int operand1);
+int pmtr(int operand1, int operand2);
+int arrq(int operand1, int operand2);
+int arsq(int operand1);
+int arci(int operand1);
+int factorial(int operand1);
+int ams(int operand1);
+int sq(int operand1);
+int cu(int operand1);
+int rev(int operand1);
+int volcube(int operand1);
+int volsp(int operand1);
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
+#endif // CALCULATOR_OPERATIONS_H_INCLUDED
