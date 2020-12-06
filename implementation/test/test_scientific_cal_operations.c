8bbbdd92adc3dacb7fed02857990d5e1e9ca3668
Genesis99003160 <75407713+Genesis99003160@users.noreply.github.com>
2020-12-06 20:22:10 +0530

    Update test_scientific_cal_operations.c

--- implementation/test/test_scientific_cal_operations.c
+++ implementation/test/test_scientific_cal_operations.c
@@ -131,5 +131,5 @@ void test_pythogoras(void)
 }
 void test_pound_to_kg(void)
 {
-    TEST_ASSERT_EQUAL_FLOAT(9.07184,test_pound_to_kg(20),134,NULL);
+    TEST_ASSERT_EQUAL_FLOAT_MESSAGE(9.07184,test_pound_to_kg(20),"EXPECTED");
 }
